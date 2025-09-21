a71a60dc998439ba4840d5117127097286be0954
Ciurean Paul <ciureanpaul2505@gmail.com>
2025-09-21 16:34:31 +0100

    test 2

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,16 +1,9 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "6.14.0"
-    }
-  }
-
-
   backend "s3" {
     bucket       = "hackathon-backend-2025"
     key          = "statefile/terraform.tfstate"
     region       = "us-east-1"
+    use_lockfile = true
   }
 }
 
